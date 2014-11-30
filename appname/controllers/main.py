9485ac970301ebd7593b9c2e1b956a2bdfbfdd9e
Roland Work - Laptop <roland@zenith.io>
2014-11-30 17:48:54 +0000

    removed user auth stuff

--- appname/controllers/main.py
+++ appname/controllers/main.py
@@ -3,7 +3,7 @@ from flask import Blueprint, render_template, flash, request, redirect, url_for
 
 from appname import cache
 from appname.forms import LoginForm
-from appname.models import User
+
 
 main = Blueprint('main', __name__)
 
