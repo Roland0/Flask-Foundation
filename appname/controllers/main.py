7a6127ecf41d2b0d662ab8c77c29ddded2de475f
Roland Work - Laptop <roland@zenith.io>
2014-11-30 18:11:28 +0000

    started doing form

--- appname/controllers/main.py
+++ appname/controllers/main.py
@@ -8,9 +8,11 @@ from appname.forms import domain_submit
 main = Blueprint('main', __name__)
 
 
-@main.route('/')
+@main.route('/', methods=['GET', 'POST'])
 @cache.cached(timeout=1000)
 def home():
-    return render_template('index.html')
+    form = domain_submit()
+
+    return render_template('index.html', form=form)
 
 
